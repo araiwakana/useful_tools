791401cd83c1cdfc071306a8b8e34799b4cd939f
Kohei909Otsuka <kohei@ootsukakouhei-no-MacBook-Pro.local>
2016-04-26 21:09:28 +0700

    [add]：chatworkの基本クラスをまとめる

--- lib/api/chatwork/room.rb
+++ lib/api/chatwork/room.rb
@@ -1,5 +1,17 @@
 module ChatWork
   class Room
-
+    attr_accessor
+    def initialize()
+    end
+    def self.all
+    end
+    def self.create()
+    end
+    def self.find()
+    end
+    def update()
+    end
+    def delete
+    end
   end
 end
